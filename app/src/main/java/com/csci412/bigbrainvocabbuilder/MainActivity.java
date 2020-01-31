01b3e4c35aa2daecd6b5d8270db94bec0f267f47
Chris Lokken <lokkenc@wwu.edu>
2020-01-31 14:55:25 -0800

    Added login and settings button to activity_main

--- app/src/main/java/com/csci412/bigbrainvocabbuilder/MainActivity.java
+++ app/src/main/java/com/csci412/bigbrainvocabbuilder/MainActivity.java
@@ -2,7 +2,9 @@ package com.csci412.bigbrainvocabbuilder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
