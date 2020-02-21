dbfd679e226b5dd5575c0e4177860a9dbc233bff
lamn2 <lamn2@wwu.edu>
2020-02-20 16:33:13 -0800

    Adjust randomword db function, and randomize test screen

--- app/src/main/java/com/csci412/bigbrainvocabbuilder/TestActivity.java
+++ app/src/main/java/com/csci412/bigbrainvocabbuilder/TestActivity.java
@@ -3,12 +3,32 @@ package com.csci412.bigbrainvocabbuilder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class TestActivity extends AppCompatActivity {
+
+
+    TestLogic tl = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        tl = new TestLogic(this);
+        setTestQuestion();
+    }
+
+    void setTestQuestion() {
+        String[] content = tl.createQuestion();
+        TextView wordView = (TextView)findViewById(R.id.testVocabWord);
+        RadioButton choice0 = (RadioButton)findViewById(R.id.testChoice0);
+        RadioButton choice1 = (RadioButton)findViewById(R.id.testChoice1);
+        RadioButton choice2 = (RadioButton)findViewById(R.id.testChoice2);
+
+        wordView.setText(content[0]);
+        choice0.setText(content[1]);
+        choice1.setText(content[2]);
+        choice2.setText(content[3]);
     }
 }
